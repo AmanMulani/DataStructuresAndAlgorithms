43a4edd08477a9e1b7d217eff9c5252506d96e09
Aman Mulani <55574632+AmanMulani@users.noreply.github.com>
2020-03-24 19:02:16 +0530

    Added memoized solution (dynamic programming)

--- finbonacci_series.py
+++ finbonacci_series.py
@@ -6,8 +6,9 @@ Question: Find the Nth term of the fibonacci series.
 
 #Without using recursion, i.e. bottom up approach
 """
-To run this program, call the bottom_up_approach() with any interger value.
-Example: bottom_up_approach(5)
+To run this program, call the bottom_up_approach() function with any interger value.
+Example: 
+print(bottom_up_approach(5))
 Output:
 5
 
@@ -30,9 +31,9 @@ def bottom_up_approach(N):
 
 #By using recursion
 """
-To run this program, call the bottom_up_approach() with any integer value.
+To run this program, call the bottom_up_approach() function with any integer value.
 Example:
-fibo_recursive(5)
+print(fibo_recursive(5))
 Output:
 5
 """
@@ -42,4 +43,25 @@ def fibo_recursive(N):
   return fibo_recursive(N-1) + fibo_recursive(N-2)
 
 
+#By using dynamic programming, memoized solution
+"""
+To run this program, call the finonacci() function with an integer value.
+Example:
+print(fibonacci(5))
+Output:
+5
+"""
 
+def fibonacci_memoized(N, memo):
+  if memo[N] != null:
+    return memo[N]
+  else if N==1 or N==2:
+    result = 1
+  elif:
+    result = fibonacci_memoized(N-1, memo) + fibonacci_memoized(N-2, memo)
+  memo[N] = result
+  return result
+
+def fibonacci(N):
+  return fibonacci_memoized(N, [None]*(N+1))
+    
